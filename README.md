3f024a1b2e9572e7b4b89fc81dcb681e80933aa6
Petr Plsek <me@kalanys.com>
2020-10-28 14:55:52 +0100

    Pager interfaces - initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Pager Interfaces
+================
+
+Contains simple interfaces for creating pagers compatible across the libraries.
+It has been cut from running project and simplified for usage available for another
+libraries.
---
+++ src/IPager.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Pager;
+
+/**
+ * Interface IPager
+ *
+ * @package Pager
+ */
+interface IPager
+{
+    /**
+     * Set maximum available results for paging
+     * @param int $maxResults
+     * @return $this
+     */
+    public function setMaxResults(int $maxResults): self;
+
+    /**
+     * Returns maximum available results for paging
+     * @return int
+     */
+    public function getMaxResults(): int;
+
+    /**
+     * Set current page number
+     * @param int $page
+     * @return $this
+     */
+    public function setActualPage(int $page): self;
+
+    /**
+     * Returns current page number
+     * @return int
+     */
+    public function getActualPage(): int;
+
+    /**
+     * Returns calculated offset
+     * @return int
+     */
+    public function getOffset(): int;
+
+    /**
+     * Returns number of available pages
+     * @return int
+     */
+    public function getPagesCount(): int;
+
+    /**
+     * Have we that page?
+     * @param int $i
+     * @return bool
+     */
+    public function pageExists(int $i): bool;
+
+    /**
+     * Set limit of items on one page
+     * @param int $limit
+     * @return $this
+     */
+    public function setLimit($limit): self;
+
+    /**
+     * Returns limit of items on one page
+     * @return int
+     */
+    public function getLimit(): int;
+}
